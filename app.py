f59ba78c98a314172c7bf610040325b0616510e7
almasharfi9977-code <almasharfi9977@gmail.com>
2026-09-04 22:04:07 +0400

    Update app.py

--- app.py
+++ app.py
@@ -40,5 +40,5 @@ def index():
 
     return render_template('index.html', student_data=student_data, error_message=error_message, search_query=search_query)
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     app.run(debug=True)
